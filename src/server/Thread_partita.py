580749d2ad22bfb83de8fd0a235a7c52f3a8fb67
Alessandro Piacentini <piacentinialessandro05@gmail.com>
2023-11-12 21:41:08 +0100

    primo commit

--- src/server/Thread_partita.py
+++ src/server/Thread_partita.py
@@ -3,5 +3,14 @@ from Giocatore import Giocatore
 
 def partita(fase_di_gioco):
     while fase_di_gioco=="game":
-        print("partita finita")
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         fase_di_gioco="waiting"
