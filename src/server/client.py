0eaa64defe0354a1b4976b5a08a4e1c7b0b2c3a4
AlessandroPiacentini <piacentinialessandro05@gmail.com>
2023-11-15 11:53:19 +0100

    commit def partita non funzina

--- src/server/client.py
+++ src/server/client.py
@@ -4,7 +4,7 @@ import socket
 server_ip = '127.0.0.1'
 server_port = 12345  # Sostituisci con la porta del server
 client_ip=server_ip
-client_port=54321
+client_port=54322
 
 # Crea una connessione al server
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -45,4 +45,30 @@ while conta<2:
     # Ricevi i dati dal client
     data = client_socket.recv(1024)
     print(f"Dati ricevuti dal client: {data.decode('utf-8')}")
+    if conta==0:
+        ip_server=data.decode('utf-8').split(";")[2]
+        porta_server=data.decode('utf-8').split(";")[1]
     conta+=1
+    server_socket.close()
+    
+
+client_socket.bind((client_ip, client_port))
+client_socket.connect((ip_server, porta_server))
+
+# Messaggio da inviare al server
+message = "busso"
+
+# Invia il messaggio al server
+client_socket.sendall(message.encode())
+
+# Ricevi la risposta dal server
+response = client_socket.recv(1024)  # Ricevi fino a 1024 byte di dati
+
+# Decodifica la risposta
+response_str = response.decode()
+
+# Stampa la risposta del server
+print("Risposta dal server:", response_str)
+
+# Chiudi la connessione al server
+client_socket.close()
