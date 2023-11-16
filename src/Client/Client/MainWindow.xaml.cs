b14123b112b7e849caaa9de63ca7e26e86a972d0
AlessandroAH <alessandroaguilar344@gmail.com>
2023-11-16 19:29:20 +0100

    Inizio Client

---
+++ src/Client/Client/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Creazione di un'istanza della seconda finestra
+            WindowPaginaDiLogin WindowLogin = new WindowPaginaDiLogin();
+
+            // Mostra la nuova finestra
+            WindowLogin.Show();
+            this.Close();
+        }
+    }
+}
---
+++ src/Client/Client/WindowPaginaDiLogin.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Logica di interazione per WindowPaginaDiLogin.xaml
+    /// </summary>
+    public partial class WindowPaginaDiLogin : Window
+    {
+        public WindowPaginaDiLogin()
+        {
+            InitializeComponent();
+        }
+        private void InvioDati()
+        {
+            try
+            {
+                client = new TcpClient("ServerIPAddress", ServerPort); // Sostituisci "ServerIPAddress" con l'indirizzo IP del server e ServerPort con la porta del server
+                stream = client.GetStream();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Errore nella connessione al server: {ex.Message}");
+            }
+
+            try
+            {
+                if (client != null && client.Connected)
+                {
+                    byte[] data = Encoding.UTF8.GetBytes(MessageTextBox.Text);
+                    stream.Write(data, 0, data.Length);
+                }
+                else
+                {
+                    MessageBox.Show("Connessione non attiva.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Errore nell'invio dei dati al server: {ex.Message}");
+            }
+        }
+
+        private void RicezioneDati()
+        {
+
+        }
+    }
+}
