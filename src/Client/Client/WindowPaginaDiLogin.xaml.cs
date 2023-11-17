0af6f39808a3f20da8f54becb38d6beb4d48999d
AlessandroAH <alessandroaguilar344@gmail.com>
2023-11-17 17:59:40 +0100

    Connessione TCP

--- src/Client/Client/WindowPaginaDiLogin.xaml.cs
+++ src/Client/Client/WindowPaginaDiLogin.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,15 +20,21 @@ namespace Client
     /// </summary>
     public partial class WindowPaginaDiLogin : Window
     {
+        private TcpClient client;
+        private NetworkStream stream;
+        private String messaggioRicevuto;
+
         public WindowPaginaDiLogin()
         {
             InitializeComponent();
         }
-        private void InvioDati()
+
+        //Invio di un messaggio al Server
+        private void InvioDati(String messaggio)
         {
             try
             {
-                client = new TcpClient("ServerIPAddress", ServerPort); // Sostituisci "ServerIPAddress" con l'indirizzo IP del server e ServerPort con la porta del server
+                client = new TcpClient("ServerIPAddress", 666); // Sostituisci "ServerIPAddress" con l'indirizzo IP del server e ServerPort con la porta del server
                 stream = client.GetStream();
 
             }
@@ -40,7 +47,7 @@ namespace Client
             {
                 if (client != null && client.Connected)
                 {
-                    byte[] data = Encoding.UTF8.GetBytes(MessageTextBox.Text);
+                    byte[] data = Encoding.UTF8.GetBytes(messaggio+";");
                     stream.Write(data, 0, data.Length);
                 }
                 else
@@ -54,9 +61,72 @@ namespace Client
             }
         }
 
-        private void RicezioneDati()
+        //ricezione di messaggio
+        private String RicezioneDati()
         {
+            try
+            {
+                if (stream.DataAvailable)
+                {
+                    byte[] buffer = new byte[1024]; // Dimensione del buffer per memorizzare i dati ricevuti
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length); // Leggi i dati ricevuti nella connessione TCP
 
+                    if (bytesRead > 0)
+                    {
+                        string receivedData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                        // Restituisci i dati ricevuti
+                        return receivedData;
+                    }
+                }
+                else
+                {
+                    // Nessun dato disponibile al momento, restituisci una stringa vuota o un valore nullo
+                    return string.Empty;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Gestisci l'eccezione
+                Console.WriteLine("Errore durante la ricezione dei dati: " + ex.Message);
+            }
+
+            // In caso di errori o dati non disponibili, restituisci una stringa vuota o un valore nullo
+            return string.Empty;
         }
+
+        //Richiesta di entrare nel gioco al server
+        private void buttonEntra_Click(object sender, RoutedEventArgs e)
+        {
+            InvioDati("entrare");
+            if (RicezioneDati()=="connesso")
+            {
+                RichiestaSuccesso();
+            }
+            else
+            {
+                RichiestaFallita();
+            }
+        }
+
+        //Se la richiesta viene accettata
+        private void RichiestaSuccesso()
+        {
+            // Creazione di un'istanza della terza finestra (finestra di gioco)
+            WindowDiGioco WindowDiGioco = new WindowDiGioco();
+
+            // Mostra la nuova finestra
+            WindowDiGioco.Show();
+            this.Close();
+        }
+
+        //Se la richiesta non viene accettata
+        private void RichiestaFallita()
+        {
+            //esce un messaggio di attesa dove comparira un counter
+            //OPZIONI DUE:
+            //OPZIONE 1 - rimane in coda e appena e disponibile una partita entra
+            //OPZIONE 2 - alla fine del counter devi schiacciare entra
+        }
+
     }
 }
