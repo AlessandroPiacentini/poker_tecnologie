d608f1341bd211bd5c2b5eda85ce92108df9f992
AlessandroAH <alessandroaguilar344@gmail.com>
2023-11-18 19:55:20 +0100

    aggiunta alcuni metodi partita

--- src/Client/Client/WindowDiGioco.xaml.cs
+++ src/Client/Client/WindowDiGioco.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,9 +20,60 @@ namespace Client
     /// </summary>
     public partial class WindowDiGioco : Window
     {
-        public WindowDiGioco()
+        private TcpClient client;
+        private NetworkStream stream;
+
+        public WindowDiGioco(TcpClient tcpClient, NetworkStream tcpStream)
         {
             InitializeComponent();
+            client = tcpClient;
+            stream = tcpStream;
+        }
+
+        private void GiocatoriSedutiAlTavolo()
+        {
+
+        }
+        private void SetupPartita()
+        {
+            /*for (int i = 0; i < 53; i++)
+            {
+                if(carta == i)
+                {
+                    img[i] = immagineresourcei;
+                }
+            
+            }*/
+        }
+        //Permette l'invio di dati al server
+        private void InvioDati(String messaggio)
+        {
+            try
+            {
+                byte[] message = Encoding.ASCII.GetBytes(messaggio + ";");
+                stream.Write(message, 0, message.Length);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Errore: " + e);
+            }
+        }
+
+        //Permette la ricezione di dati dal server
+        private String RicezioneDati()
+        {
+            try
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                String response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                return response.Trim(); // Rimuove eventuali spazi bianchi
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Errore nella ricezione dei dati: " + e);
+                return "err";
+            }
         }
     }
 }
