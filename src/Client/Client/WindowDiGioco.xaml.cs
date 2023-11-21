012031eb10bb4ed1c0a751980c67d8c9bb22d20d
AlessandroAH <alessandroaguilar344@gmail.com>
2023-11-21 17:45:37 +0100

    aggiunta degli ultimi metodi

--- src/Client/Client/WindowDiGioco.xaml.cs
+++ src/Client/Client/WindowDiGioco.xaml.cs
@@ -38,6 +38,9 @@ namespace Client
         int grande_buio = 0;
         int piccolo_buio = 0;
 
+        //Contatore dei turni
+        int conta = 0;
+
         public WindowDiGioco(TcpClient tcpClient, NetworkStream tcpStream)
         {
             InitializeComponent();
@@ -46,23 +49,16 @@ namespace Client
 
             IniziaGioco();
         }
-
+    
         //INIZIO DEL GIOCO
         private void IniziaGioco()
         {
-
-            //check (fold)
-
-            //puntata
-
-            //abbandonare
-
             CarteIniziali();
             CarteSulTavolo();
+            addBottoneAlGiocatore();
 
-            //continua fino a quando il gioco non è finito
-            //while (RicezioneDati()=="partita_finita")
-
+            if (conta == 4) aggiungiEVisualizzaCarta4();
+            if (conta == 5) aggiungiEVisualizzaCarta5();
         }
 
 
@@ -113,13 +109,39 @@ namespace Client
             //DISATTIVA TUTTO
         }
 
+        //Passo il turno
+        private void check()
+        {
+            InvioDati("Check");
+            if (RicezioneDati() == "ok_fermo")
+            {
+                //disattiva i bottoni
+                buttonFold.IsEnabled = false;
+                buttonCheck.IsEnabled = false;
+                buttonPuntata.IsEnabled = false;
+            }
+        }
+
+        //Disabilita tutto perchè hai finito il turno
+        private void turnoFinito()
+        {
+            InvioDati("turno_finito");
+            if (RicezioneDati() == "ok_fermo")
+            {
+                //disattiva i bottoni
+                buttonFold.IsEnabled = false;
+                buttonCheck.IsEnabled = false;
+                buttonPuntata.IsEnabled = false;
+            }
+        }
+
         //Metodo della puntata che invia al Server
-        /*private void puntata()
+        private void puntata()
         {
             String puntata = "";
 
             //Metti in una variabile la puntata
-            puntata = int.Parse(txtPuntata.text);
+            puntata = txtPuntata.Text;
 
             //invia i soldi della puntata
             InvioDati("puntata");
@@ -134,15 +156,45 @@ namespace Client
                 {
                     InvioDati(puntata);
                     //E infine aggiorna il saldo
-                    txtSaldo.Text = int.Parse(txtSaldo.text) - int.Parse(txtPuntata.text);
+                    txtSaldo.Text = (int.Parse(txtSaldo.Text) - int.Parse(txtPuntata.Text)).ToString();
                 }
             }   
-        }*/
+        }
         
        
-        
+
+        //CAMBIAMENTO SUL CAMPO
+
+        //Fa vedere la nuova carta sul tavolo (carta 4)
+        private void aggiungiEVisualizzaCarta4()
+        {
+            InvioDati("pesca");
+            String[] carte = datiSplittati();
+
+            //Rendo la immagine di nuovo visibile
+            imgTavolo4.Opacity = 0.5;
+
+            //E cambio percorso
+            imgTavolo4.Source = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "//immagini/" + carte[1] + ".jpg"));
+        }
+
+        //Fai vedere la nuova e quinta carta sul tavolo
+        private void aggiungiEVisualizzaCarta5()
+        {
+            InvioDati("pesca");
+            String[] carte = datiSplittati();
+
+            //Rendo la immagine di nuovo visibile
+            imgTavolo4.Opacity = 0.5;
+
+            //E cambio percorso
+            imgTavolo5.Source = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "//immagini/" + carte[1] + ".jpg"));
+        }
+
+
+
         //METODI PER IL SETUP DEL GIOCO
-        
+
         //Carte sul tavolo
         private void CarteSulTavolo()
         {
@@ -155,6 +207,10 @@ namespace Client
             imgTavolo1.Source = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "//immagini/" + carte[0] + ".jpg"));
             imgTavolo2.Source = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "//immagini/" + carte[1] + ".jpg"));
             imgTavolo3.Source = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "//immagini/" + carte[2] + ".jpg"));
+
+            //Le altre carte le rendo trasparenti
+            imgTavolo4.Opacity = 0;
+            imgTavolo5.Opacity = 0;
         }
 
         //Ovvero dai a ciascun giocatore le carte che gli spettano decide dal server
@@ -236,6 +292,23 @@ namespace Client
 
         }
 
+        //Assegnazione dei bottoni al giocatore
+        private void addBottoneAlGiocatore()
+        {
+            InvioDati("giocatore");
+            String[] g = datiSplittati();
+
+            // Supponiamo che 'giocatore' sia il nome di un elemento della UI di tipo Grid
+            Grid giocatore = this.FindName(g[0]) as Grid;
+
+            if (giocatore != null)
+            {
+                giocatore.Children.Add(buttonCheck); // Aggiungi il pulsante alla griglia
+                giocatore.Children.Add(buttonFold); // Aggiungi il pulsante alla griglia
+                giocatore.Children.Add(buttonFold); // Aggiungi il pulsante alla griglia
+            }
+        }
+
 
 
         //METODI UTILI DURANTE LA CONNESSIONE TRA CLIENT E SERVER
@@ -278,6 +351,88 @@ namespace Client
 
             String dati = RicezioneDati().ToString(); // Ottieni i dati come stringa
             return carte = dati.Split(';'); // Dividi la stringa utilizzando il punto e virgola come separatoreuna virgola
+        }
+
+
+        //METODI ASCINCORNI PER LA COMUNICAZIONE CON IL SERVER
+        //Creazione di un metodo asincrono per l'attesa del messaggio dal server:
+        private async Task<String> AttendiTurno()
+        {
+            string risposta = "";
+
+            while (true)
+            {
+                risposta = RicezioneDati();
+                if (risposta == "il_tuo_turno")
+                {
+                    break; // Esci dal ciclo quando è di nuovo il tuo turno
+                }
+
+                // Aggiungi un ritardo per evitare un ciclo troppo veloce
+                await Task.Delay(1000); // Ritardo di 1 secondo (1000 millisecondi)
+            }
+
+            return risposta;
+        }
+
+        //METODI DEI BOTTONI
+
+        //Se viene schiacciato il tasto del FOLD
+        private async void buttonFold_Click(object sender, RoutedEventArgs e)
+        {
+            abbandonare();
+            turnoFinito();
+
+            // Aspetta fino a quando non è di nuovo il tuo turno
+            String messaggio = await AttendiTurno();
+
+            // Qui puoi eseguire azioni dopo aver ricevuto il messaggio "il_tuo_turno"
+            if (messaggio == "il_tuo_turno")
+            {
+                // Abilita di nuovo i bottoni
+                buttonFold.IsEnabled = true;
+                buttonCheck.IsEnabled = true;
+                buttonPuntata.IsEnabled = true;
+            }
+        }
+
+        //Se viene schiacciato il tasto check
+        private async void buttonCheck_Click(object sender, RoutedEventArgs e)
+        {
+            check();
+            turnoFinito();
+
+            // Aspetta fino a quando non è di nuovo il tuo turno
+            String messaggio = await AttendiTurno();
+
+            // Qui puoi eseguire azioni dopo aver ricevuto il messaggio "il_tuo_turno"
+            if (messaggio == "il_tuo_turno")
+            {
+                // Abilita di nuovo i bottoni
+                buttonFold.IsEnabled = true;
+                buttonCheck.IsEnabled = true;
+                buttonPuntata.IsEnabled = true;
+            }
+        }
+
+        //Se viene schiacciato il tasto puntata
+        private async void buttonPuntata_Click(object sender, RoutedEventArgs e)
+        {
+            puntata();
+            turnoFinito();
+
+            // Aspetta fino a quando non è di nuovo il tuo turno
+            String messaggio = await AttendiTurno();
+
+            // Qui puoi eseguire azioni dopo aver ricevuto il messaggio "il_tuo_turno"
+            if (messaggio == "il_tuo_turno")
+            {
+                // Abilita di nuovo i bottoni
+                buttonFold.IsEnabled = true;
+                buttonCheck.IsEnabled = true;
+                buttonPuntata.IsEnabled = true;
+            }
+
         }
     }
 }
